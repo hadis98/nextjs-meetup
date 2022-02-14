be9564244b5aabe86dc40b4bf7c85e2c12b0eee2
hadis ghafouri <hadisghafouri98@gmail.com>
2022-02-14 21:24:17 +0330

    send new meetup to mongodb in new-meetup page

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,20 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
-
+import { useRouter } from "next/router";
 const NewMeetupPage = () => {
-  const addMeetupHandler = (enteredMeetupData) => {
+  const router = useRouter();
+  const addMeetupHandler = async (enteredMeetupData) => {
     console.log(enteredMeetupData);
+    // we have an internal api
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    router.replace("/");
   };
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 };
